625000e668069eb8878bdce664b7916a08452622
Haksun Kim <itpro@haksunkim.com>
2017-11-01 23:13:17 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Personal Library
+This is small personal project to maintain ebooks in local isolated network (not for public) for search and download to ebook readers.
---
+++ src/main/java/com/haksunkim/personallibrary/PersonalLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.haksunkim.personallibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalLibraryApplication.class, args);
+	}
+}
